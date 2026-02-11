f15368cd71669f6d609052a64b524511eacfe757
codebraver73-lab <codebraver73@codebrave.org>
2026-02-11 03:20:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+ const user={
+    name:"joud",
+    age:"17",
+    city:"Rashaya",
+};
+
+const {name,age,city}=user;
+
+
+document.getElementById("name").textContent="Name: "+name;
+document.getElementById("age").textContent="Age: "+age;
+document.getElementById("city").textContent="City: "+city;
